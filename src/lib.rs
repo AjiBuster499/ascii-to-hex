3463511fa0a2de53d51c7f2c5556ba8bd2a30ec6
Samuel A. Mikell <sbug1442@gmail.com>
2022-02-10 17:25:23 -0500

    Finished. Added Tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,25 +23,43 @@ pub mod ascii_to_hex {
     /// This function will convert each u8 slice element
     /// into a String element containing the hex representation
     /// of the u8 element
-    fn convert_to_hex<'a>(ascii_bytes: &'a [u8]) -> Vec<&'a str> {
-        ascii_bytes.into_iter().map(|byte| {}).collect()
+    fn convert_to_hex<'a>(ascii_bytes: &'a [u8]) -> Vec<String> {
+        ascii_bytes
+            .into_iter()
+            .map(|byte| format!("{:X}", byte).as_str().to_owned())
+            .collect()
     }
     /// This function simply concatenates all the hex strings
-    fn concatenate_slices<'a>(hex_vec: Vec<&'a str>) -> String {
-        let mut result = String::new();
-        for hex_slice in hex_vec {
-            result.push_str(hex_slice);
-        }
-
-        result
+    fn concatenate_slices<'a>(hex_vec: Vec<String>) -> String {
+        hex_vec.concat()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::ascii_to_hex;
+
     #[test]
     fn it_works() {
         let test_string = "test";
-        assert_eq!(test_string, "test");
+        assert_eq!(ascii_to_hex::get_hex_string(test_string), "74657374");
+    }
+
+    #[test]
+    fn empty_string() {
+        let test_string = "";
+        assert_eq!(ascii_to_hex::get_hex_string(test_string), "");
+    }
+
+    #[test]
+    fn long_string() {
+        let test_string = "DSJAKLFHASJKGLF879Q472631987HJKFSDAHFLKSJADHsdahlkfjhdskalfh";
+        assert_eq!(ascii_to_hex::get_hex_string(test_string), "44534A414B4C464841534A4B474C4638373951343732363331393837484A4B4653444148464C4B534A414448736461686C6B666A6864736B616C6668");
+    }
+
+    #[test]
+    fn special_characters() {
+        let test_string = "!@#%";
+        assert_eq!(ascii_to_hex::get_hex_string(test_string), "21402325");
     }
 }
